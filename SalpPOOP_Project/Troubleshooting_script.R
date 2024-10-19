1180bbcedfbdf196f1e2b7ba63eb94b8087a29c6
hbudroe <hmb25721@uga.edu>
2024-10-19 12:25:04 -0400

    updating analysis scripts to start from scratch analyses on new mac (read in csvs...)

--- SalpPOOP_Project/Troubleshooting_script.R
+++ SalpPOOP_Project/Troubleshooting_script.R
@@ -65,3 +65,41 @@ dev.off()
 #but it saved the file as the folder (since wouldn't let me put /name), and opening is text string???
 
 
+
+#Compositional tree plots Cordier 2a ----
+
+#library('doMC')
+#registerDoMC(cores = 16)
+library(ggplot2)
+library(ggpubr)
+library(treemapify)
+library(seqinr)
+
+#notes
+
+
+pdf("Figure2/Figure_2_Treemap_richness_eupthotic.pdf", width = 8, height = 2) 
+treemap::treemap(subset(df_rich_agg, df_rich_agg$variable == "Euphotic"), 
+                 title = "Richness - Euphotic", 
+                 algorithm = "pivotSize", border.lwds = c(1,0.5,0.1), 
+                 border.col = c("black", "black", "black"),
+                 mapping = c(0,0,0),
+                 index=c("rank3","rank4", "rank5"),
+                 vSize="value",
+                 vColor="rank_col",
+                 type="index",
+                 fontsize.labels=c(18,14,10),             # size of labels. Give the size per level of aggregation: size for group, size for subgroup, sub-subgroups...
+                 fontcolor.labels=c("Grey", "orange","white"),    # Color of labels
+                 fontface.labels=c(3,2,1),                # Font of labels: 1,2,3,4 for normal, bold, italic, bold-italic...
+                 bg.labels=c("transparent"),              # Background color of labels
+                 align.labels=list(
+                   c("left", "top"), 
+                   c("left", "bottom"),
+                   c("center", "center")
+                 ),                                   # Where to place labels in the rectangle?
+                 overlap.labels=0.8,                  # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
+                 inflate.labels=F,
+                 force.print.labels = F) 
+dev.off()
+
+
